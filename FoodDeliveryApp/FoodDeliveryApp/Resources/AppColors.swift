b72c6748250496a90713f7fbac6e62aed08c52c3
Василий Цветков <tsvvasek@Vasilijs-Laptop.local>
2024-03-16 16:23:09 +0300

    add app colors

---
+++ FoodDeliveryApp/FoodDeliveryApp/Resources/AppColors.swift
@@ -0,0 +1,15 @@
+//
+//  AppColors.swift
+//  FoodDeliveryApp
+//
+//  Created by Василий Цветков on 16.03.2024.
+//
+
+import UIKit
+
+enum AppColors {
+    static let accentOrange = UIColor().hex(0xD35400)
+    static let grey = UIColor().hex(0xECF0F1)
+    static let black = UIColor().hex(0x000000)
+    static let background = UIColor().hex(0xE5E5E5)
+}
