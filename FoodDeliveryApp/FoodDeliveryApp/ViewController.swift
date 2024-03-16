c50f2cd5666ece95460c3828f991cb99e7626a02
Василий Цветков <tsvvasek@Vasilijs-Laptop.local>
2024-03-16 12:28:02 +0300

    created base project

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Василий Цветков on 16.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
